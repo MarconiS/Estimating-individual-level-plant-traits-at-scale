656cbfe4e5e3437c165c66913d770e70160621df
MarconiS <s.marconi@ufl.edu>
2019-10-23 10:42:39 -0400

    up

--- R/calculate_model_performance.R
+++ R/calculate_model_performance.R
@@ -5,16 +5,31 @@
 #' @export
 #' @examples
 #' @importFrom magrittr "%>%"
-calculate_model_performance <- function(trait = "P_pct", snap_path = "", get_best = F){
+calculate_model_performance <- function(trait = "P_pct", snap_path = "", nbags = 1){
   #retrieve number of snaps
-  loops <- list.files("./outdir/snaps/") %>% length
+  loops <- list.files("./outdir/PBMs", pattern = ".rds", full.names = T)
 
-  #get delta_aic and rank best models
-  mod.aic <- rep(NA, loops)
-  for(bb in 1: length(mod.aic)){
-    foo <- readRDS()
-    mod.aic[bb] <- foo[[bb]]$score$aic
-   }
-  mask <- cbind(which(mod.aic %in% head(sort(mod.aic), 100)), mod.aic[mod.aic %in% head(sort(mod.aic), 100)])
+  #check if Ensemble model has been created yet
+  model_stack <- file.exists(paste("./outdir/EPBMs/", trait, ".rds", sep=""))
+
+  if(!model_stack || length(model_stack) != nbags){
+    #get delta_aic and rank best models
+    mod.aic <- rep(NA, length(loops))
+    model_stack <- NULL
+    for(bb in 1: length(mod.aic)){
+      foo <- readRDS(loops[bb])
+      mod.aic[bb] <- foo$mod$AIC[foo$ncomp]
+      #create model stack with all random bags models
+      model_stack[[bb]] <-foo
+    }
+    #subset the best n models to be used for performance
+    mask <- cbind(which(mod.aic %in% head(sort(mod.aic), nbags)),
+                  mod.aic[mod.aic %in% head(sort(mod.aic), nbags)])
+    #save the model ensemble in an R object
+    saveRDS(model_stack[mask[,1]],
+            file = paste("./outdir/EPBMs/", trait, ".rds", sep=""))
+  }else{
+    model_stack <- readRDS(paste("./outdir/EPBMs/", trait, ".rds", sep="")))
+  }
 
 }
