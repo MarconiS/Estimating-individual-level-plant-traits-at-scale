76304979d74ad0dce66efd2023b153be17fac1d8
MarconiS <s.marconi@ufl.edu>
2020-06-30 15:12:57 -0400

    Update pls_glm.R

--- R/pls_glm.R
+++ R/pls_glm.R
@@ -79,11 +79,34 @@ pls_glm <- function(ll = NULL, trait = NULL, nrmlz=F){
 
     #store validation metrics
     Y.test <- as.vector(test.data[,names(test.data) %in% trait])
-    out[["pred"]] <- (predict(mod, newdata=X.tst,
+    out[["pred"]] <- exp(pls_glm_predict(mod, newdata=X.tst,
                              type="response",comps=as.integer(out["ncomp"])))
-    out[["pR2"]] <- 1 - sum((out[["pred"]] - (Y.test))^2) /
+    out[["pR2"]] <- 1 - sum((out[["pred"]][,1] - (Y.test))^2) /
       sum((Y.test - mean(Y.test))^2)
     out[["mod"]] <- mod
     saveRDS(out, paste("./outdir/PBMs/pls_glm_", trait, ll, ".rds", sep=""))
   return(out)
 }
+
+pls_glm_predict <- function(object,newdata,
+                            comps=object$computed_nt, type=c("link", "response", "terms", "scores", "class", "probs"),
+                            se.fit=FALSE, wt = NULL, dispersion = NULL,methodNA="adaptative",verbose=TRUE,...)
+{
+  nrnd <- nrow(newdata)
+  newdataNA <- !is.na(newdata)
+  newdata <- sweep(sweep(newdata, 2, attr(object$ExpliX, "scaled:center")),
+                   2, attr(object$ExpliX, "scaled:scale"), "/")
+  newdata <- as.matrix(newdata)
+  #newdata[!newdataNA] <- 0
+  newdata = lapply(1:nrnd, function(x)c(newdata[x,] %*% object$wwetoile[, 1:comps],
+                                        rep(0, object$computed_nt - comps)))
+  newdata = do.call(rbind, newdata)
+  colnames(newdata) <- NULL
+  newdata <- data.frame(tt = newdata)
+  pred_int = HH::interval(object$FinalModel, newdata=newdata, type="response")
+  pred_int = pred_int[,c(1,4,5)]
+  colnames(pred_int) = c("fit","lwr","upr")
+  return(pred_int[])
+}
+
+
